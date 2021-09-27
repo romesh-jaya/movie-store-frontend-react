df4a0d998f6b0098a6aed987f2198ce9b03fe598
romesh-jaya <56665179+romesh-jaya@users.noreply.github.com>
2021-09-27 10:22:59 +0530

    feat: add lint plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,44 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
   extends: [
     'eslint:recommended',
-    'prettier'
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
   ],
   env: {
     node: true,
-    es6: true
+    es6: true,
   },
   rules: {
     'no-console': 'off',
-    '@typescript-eslint/no-magic-numbers': 'off',
     'react/prop-types': 0,
     'react/jsx-props-no-spreading': 'off',
-    'brace-style': "error"
+    'brace-style': 'error',
+    'react/jsx-indent': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-unused-vars-experimental': 'off',
+    '@typescript-eslint/no-magic-numbers': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'function',
+        format: ['PascalCase', 'camelCase'],
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'arrow-body-style': 0,
+    'react/display-name': 'off',
+    'no-var': 'error',
   },
-  parserOptions: {
-    sourceType: 'module'
-  }
+  plugins: ['@typescript-eslint'],
 };
